bbd5d501ba16c52895fffdcd30f4f0cadc41e3f3
fearhunt <firhanmahdi@gmail.com>
2021-06-05 22:07:43 +0700

    complete detail

--- app/src/main/java/com/example/kotlinmoviecatalogue/ShowsDetailActivity.kt
+++ app/src/main/java/com/example/kotlinmoviecatalogue/ShowsDetailActivity.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlinmoviecatalogue.databinding.ActivityShowsDetailBinding
 import com.example.kotlinmoviecatalogue.vm.ShowsDetailViewModel
+import java.text.NumberFormat
+import java.util.*
 
 class ShowsDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityShowsDetailBinding
@@ -29,14 +32,48 @@ class ShowsDetailActivity : AppCompatActivity() {
             getShowsDetailData().observe(this@ShowsDetailActivity, { shows ->
                 if (shows != null) {
                     val img = resources.getIdentifier(shows.poster, "drawable", packageName)
+                    var tags = ""
 
                     with(binding) {
                         imgPoster.setImageResource(img)
                         tvShowsTitle.text = shows.title
                         tvShowsReleaseYear.text = shows.release_year
                         tvShowsOverview.text = shows.overview
+                        tvShowsLanguage.text = shows.language
                         tvShowsScore.text = "${shows.score}%"
                         scoreBar.progress = shows.score
+
+                        if (showsType == "movies") {
+                            val currencyInstance = NumberFormat.getCurrencyInstance()
+                            currencyInstance.maximumFractionDigits = 0
+                            currencyInstance.currency = Currency.getInstance("USD")
+
+                            tvShowsBudget.text = currencyInstance.format(shows.budget)
+                            tvShowsRevenue.text = currencyInstance.format(shows.revenue)
+                        }
+                        else {
+                            tvBudget.visibility = View.GONE
+                            tvShowsBudget.visibility = View.GONE
+                            tvRevenue.visibility = View.GONE
+                            tvShowsRevenue.visibility = View.GONE
+                        }
+
+                        (shows.tags).forEachIndexed { index, tag ->
+                            var capitalizedTag = ""
+                            val tempTag = tag.split(" ")
+
+                            tempTag.forEach {
+                                capitalizedTag += it.capitalize() + " "
+                            }
+
+                            tags += capitalizedTag.trimEnd()
+
+                            if (index < ((shows.tags).size - 1)) {
+                                tags += ", "
+                            }
+                        }
+
+                        tvShowsTags.text = tags
                     }
                 }
             })
